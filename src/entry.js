0df268faec17694a5fea92f7c3e3ff60b90e8d7c
Arif Sardar <as2048282@gmail.com>
2022-02-17 18:27:13 +0530

    xeon js

---
+++ src/entry.js
@@ -0,0 +1,5 @@
+import createRealElement from "./modules/methods/real_element";
+
+console.time("Create Real Element");
+console.log(createRealElement("h1"));
+console.timeEnd("Create Real Element");
